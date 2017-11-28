a3506d763d874b5771c0598ed06fbfff40d0cc12
Jean-Claude Charrier <jcharrie@ucsc.edu>
2017-11-27 19:48:26 -0800

    HelloComonent()

--- HelloJava.java
+++ HelloJava.java
@@ -1,12 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class HelloJava{
   public static void main(String[] args){
     JFrame frame = new JFrame("Hello, Java!");
-    JLabel label = new JLabel("Hello, Java!", JLabel.CENTER);
-    frame.add(label);
+    // JLabel label = new JLabel("Hello, Java!", JLabel.CENTER);
+    // frame.add(label);
+    frame.add(new HelloComponent());
     frame.setSize(300, 300);
     frame.setVisible(true);
     // System.out.println("Hello, Java!");
   }
 }
+
+
+// class HelloComponent extends JComponent{
+//   public void paintComponent(Graphics g){
+//     g.drawString("Hello, Java!", 125, 95);
+//   }
+// }
