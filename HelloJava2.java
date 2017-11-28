e431578920338fcd6ff365a1a188a2f44e32ae1a
Jean-Claude Charrier <charrierjeanclaude@yahoo.com>
2017-11-28 03:20:36 -0800

    HelloJava2

---
+++ HelloJava2.java
@@ -0,0 +1,14 @@
+// file: HelloJava2.java
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class HelloJava2{
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("HelloJava2");
+    frame.add(new HelloComponent2("Hello, Java!"));
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(300, 300);
+    frame.setVisible(true);
+  }
+}
