88266518b5ad893df6f3804b335a0352b70fe8e1
Jean-Claude Charrier <jcharrie@ucsc.edu>
2017-11-28 19:53:09 -0800

    HelloJava2 args
    Can type text to fill parameters

--- HelloJava2.java
+++ HelloJava2.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 public class HelloJava2{
   public static void main(String[] args) {
     JFrame frame = new JFrame("HelloJava2");
-    frame.add(new HelloComponent2("Hello, Java!"));
+    frame.add(new HelloComponent2(args[0]));
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(300, 300);
     frame.setVisible(true);
